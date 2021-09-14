b1c1da2f59a0119a974e377d57ba16676db3ac06
Eshaan Agarwal <eshaan060202@gmail.com>
2021-09-14 18:03:32 +0530

    remove eslint no spreading error
    Signed-off-by: Eshaan Agarwal <eshaan060202@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 
-/* eslint-disable react/jsx-props-no-spreading */
 function MyApp({ Component, pageProps }: AppProps) {
 	return <Component {...pageProps} />
 }
